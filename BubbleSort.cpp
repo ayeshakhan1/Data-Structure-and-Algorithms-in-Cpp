c9fe006035f3af9f71f3035643b1b062857bbb97
Ayesha Khan <74055769+ayeshakhan1@users.noreply.github.com>
2023-09-23 13:17:20 +0500

    Update BubbleSort.cpp

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -1,27 +1,40 @@
 // ============================[ B U B B L E    S O R T ]============================ //
 #include<iostream>
+
 using namespace std;
 
-void BubbleSort(int *Array, int N){
-    int temp;
-    int swapped = 1;
+void BubbleSort(int * Array, int N) {
+  int temp;
+  int swapped = 1;
 
-    while (swapped > 0 ) {
-        swapped = 0;
-        
-        for(int i=0; i < N-1; i++)
-            if ( Array[i] > Array[i+1] ) {
-                temp = Array[i];
-                Array[i] = Array[i+1];
-                Array[i+1] = temp;
-                swapped = i;    }   }   }
+  while (swapped > 0) {
+    swapped = 0;
 
-int main(){
-    int size = 5;
-    int array[size] = {40, 10, 30, 20, 50};
+    for (int i = 0; i < N - 1; i++)
+      if (Array[i] > Array[i + 1]) {
+        temp = Array[i];
+        Array[i] = Array[i + 1];
+        Array[i + 1] = temp;
+        swapped = i;
+      }
+  }
+}
 
-    BubbleSort(array, size);
+int main() {
+  int size = 5;
+  int array[size] = {
+    40,
+    10,
+    30,
+    20,
+    50
+  };
 
-    for(int i = 0; i<size; i++){    cout<<array[i]<<"\t";   }
+  BubbleSort(array, size);
 
-    return 0;   }
+  for (int i = 0; i < size; i++) {
+    cout << array[i] << "\t";
+  }
+
+  return 0;
+}
